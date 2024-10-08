6fc6ba0bda3216e2701aaa7d744084b4500ad578
Abhay Joshi <abhay.joshi@tiket.com>
2024-10-08 03:32:46 +0000

    feat: added chages for supporting new version of logback encoder

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,37 +1,18 @@
 package com.example.demo;
 
-import com.tiket.tix.lib.logger.TixLogger;
-import com.tiket.tix.lib.logger.TixLoggerImpl;
-import org.springframework.boot.CommandLineRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import java.util.HashMap;
-import java.util.Map;
+
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner demo(TixLogger logger) {
-        return args -> {
-            Map<String, Object> sdc = new HashMap<>();
-            sdc.put("key", "value");
-
-            logger.info("This is an info message", sdc, null);
-            logger.debug("This is a debug message", sdc, "12345");
-            logger.warn("This is a warn message", sdc, "12345");
-            logger.error("This is an error message", "Error details", sdc, "12345");
-        };
-    }
-
-    @Bean
-    public TixLogger tixLogger() {
-        return new TixLoggerImpl();
+        Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+        logger.atInfo().addKeyValue("x_request_id", "123456").log("Booking id {} confirmed for transaction {}", "1234", "5678");
     }
 }
